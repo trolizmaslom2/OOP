dc3d208e5d1a5a47ea748adad2c230a68d21483e
Mykola Gotsaliuk <74303166+trolizmaslom2@users.noreply.github.com>
2024-02-13 13:58:33 +0200

    lb1

---
+++ LR1/code2.ts
@@ -0,0 +1,44 @@
+
+abstract class Exercise {
+    abstract getDescription(): string;
+}
+
+// група "ноги"
+class Legs extends Exercise {
+    getDescription(): string {
+        return 'Legs exercise';
+    }
+}
+
+// група "руки"
+class Arm extends Exercise {
+    getDescription(): string {
+        return 'Arm exercise';
+    }
+}
+
+// fбстрактна фабрика для створення вправ
+abstract class ExerciseFactory {
+    abstract createExercise(): Exercise;
+}
+
+// фабрика для створення вправ ніг
+class LegsFactory extends ExerciseFactory {
+    createExercise(): Exercise {
+        return new Legs();
+    }
+}
+
+// фабрика для створення вправ рук
+class ArmFactory extends ExerciseFactory {
+    createExercise(): Exercise {
+        return new Arm();
+    }
+}
+
+// Приклад використання абстрактної фабрики
+const legs = new LegsFactory();
+console.log(legs.createExercise().getDescription());
+
+const arms = new ArmFactory();
+console.log(arms.createExercise().getDescription());
